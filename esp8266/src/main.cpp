35dc52b5bde90678b1db0ce38579427acc8155ed
Juanes <johannstephan239@me.com>
2022-11-28 09:09:00 -0500

    Code modified for the 0.91 OLED display and the ESP pinout

--- esp8266/src/main.cpp
+++ esp8266/src/main.cpp
@@ -7,19 +7,21 @@ Made with love in Colombia 🇨🇴
 *******************************************************/
 
 // Libraries
-#include <SevSeg.h>
 #include <DHT.h>
-#include <WiFiNINA.h>
+#include <ESP8266WiFi.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 // Headers
-#include "secret.h"
+#include <secret.h>
 
 // Pin definitions
-#define SENSOR_PIN 17
+#define SENSOR_PIN 0
 
-// Temp Sensor & 4 digit seven segment display instances
+// Temp Sensor
 DHT dht(SENSOR_PIN, DHT11);
-SevSeg sevseg; //Instantiate a seven segment object
 
 // Variable definition
 long sensor_timer = 250;
@@ -35,12 +37,61 @@ unsigned long previousMillisInfo = 0;     //will store last time Wi-Fi informati
 unsigned long previousMillisLED = 0;      // will store the last time LED was updated
 const int intervalInfo = 5000;            // interval at which to update the board information
 
+// Display class
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+static const unsigned char PROGMEM logo_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000 };
+
+void draw_bitmap(void) {
+  display.clearDisplay();
+
+  display.drawBitmap(
+    (display.width()  - LOGO_WIDTH ) / 2,
+    (display.height() - LOGO_HEIGHT) / 2,
+    logo_bmp, LOGO_WIDTH, LOGO_HEIGHT, 1);
+  display.display();
+  delay(1000);
+}
+
 void setup() {
   // Serial port
   Serial.begin(9600);
 
+  // Display, definitions in the display_functions.h file
+  Serial.println("Setting up display");
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  // Clear the buffer
+  display.clearDisplay();
+  display.drawPixel(10, 10, SSD1306_WHITE);  // Draw a single pixel in white
+  // Show the display buffer on the screen. You MUST call display() after
+  // drawing commands to make them visible on screen!
+  display.display();
+
   // WiFi LED initialization
-  pinMode(21, OUTPUT);
+  pinMode(D4, OUTPUT);
 
   // attempt to connect to Wi-Fi network:
   while (status != WL_CONNECTED) {
@@ -55,22 +106,8 @@ void setup() {
   Serial.print("Connected to network ");
   Serial.println(ssid);
 
-  // Display
-  byte numDigits = 4;
-  byte digitPins[] = {2, 3, 4, 5};
-  byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 14};
-  bool resistorsOnSegments = false; // 'false' means resistors are on digit pins
-  byte hardwareConfig = COMMON_CATHODE; // See README.md for options
-  bool updateWithDelays = false; // Default 'false' is Recommended
-  bool leadingZeros = true; // Use 'true' if you'd like to keep the leading zeros
-  bool disableDecPoint = false; // Use 'true' if your decimal point doesn't exist or isn't connected. Then, you only need to specify 7 segmentPins[]
-  sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments,
-  updateWithDelays, leadingZeros, disableDecPoint);
-
-  // Set display brightness
-  sevseg.setBrightness(10);
-
   // Start sensor
+  Serial.println("Starting sensor");
   dht.begin();
 }
 
@@ -80,7 +117,6 @@ void loop() {
   long current_time = millis();
   if (current_time - prev_time >= sensor_timer){
     
-    sevseg.refreshDisplay();
     // Reset timer
     prev_time = current_time;
     
@@ -96,6 +132,5 @@ void loop() {
       prev_reading = feels_like;
     }
   }
-  sevseg.setNumberF(feels_like, 2);
-  sevseg.refreshDisplay();
+  // Here, display the value
 }
